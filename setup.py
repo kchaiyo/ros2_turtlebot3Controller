e337672cb570126c61a7da0925fe3803e76f5831
kchaiyo <90701050+kchaiyo@users.noreply.github.com>
2020-08-24 15:28:54 +0000

    fix executable name

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-		'turtlebot3_controller = turtlebot3_controller.turtlebot_controller:main'
+		'controller = turtlebot3_controller.turtlebot3_controller:main'
         ],
     },
 )
